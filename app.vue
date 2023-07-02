7de93d5503b0f7fc189cfab1a488fe88e1ad8eb5
Tim Benniks <tbenniks@gmail.com>
2023-07-02 12:14:46 +0200

    first commit

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Data Sets</h1>
+    <pre>{{ datasets }}</pre>
+
+    <h1>Goals</h1>
+    <pre>{{ goals }}</pre>
+  </div>
+</template>
+<script lang="ts" setup>
+const { data: datasets } = await useAsyncGql("datasets");
+const { data: goals } = await useAsyncGql("goals");
+</script>
